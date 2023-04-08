1e0bb2d2eb22c37fd3d38d6b85779df957038c3c
jjanmo <jjanmo07@hanmail.net>
2023-04-08 23:46:06 +0900

    feat: update header/layout of styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,32 @@
-import logo from 'logo.webp';
 import Image from 'next/image';
+import Link from 'next/link';
+import { BsShare } from 'react-icons/bs';
+import { VscFeedback } from 'react-icons/vsc';
 
 export default function Header() {
   return (
-    <div>
-      <Image src={logo} alt="logo" width={100} height={100} />
+    <div className="w-full h-24 flex justify-between items-center bg-blue-100">
+      <div className="relative w-24 h-24 ml-4">
+        <Link href="/">
+          <Image src={'/cute.png'} alt="logo" fill />
+        </Link>
+      </div>
+      <h1></h1>
+
+      <div className="flex">
+        <BsShare
+          size="45"
+          color="white"
+          className="p-3 mx-2 shadow-lg rounded-xl bg-blue-300 "
+        />
+        <Link href="feedback">
+          <VscFeedback
+            size="45"
+            color="white"
+            className="p-3 ml-2 mr-4 shadow rounded-xl bg-blue-300"
+          />
+        </Link>
+      </div>
     </div>
   );
 }
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,7 +4,7 @@ interface Props {
 }
 export default function Layout({ children }: Props) {
   return (
-    <div>
+    <div className="w-full max-w-lg h-screen m-auto bg-slate-100">
       <Header />
       {children}
     </div>
