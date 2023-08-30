7422ee5b512735f180260f3f54391b879685289a
jjanmo <jjanmo07@hanmail.net>
2023-08-30 18:04:31 +0900

    fix: fix getstaticprops error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,14 @@ import Layout from '@components/Layout';
 import MapSection from '@components/MapSection';
 import { Store } from '@/types/store';
 import axios from 'axios';
-import { useEffect } from 'react';
+import Error from 'next/error';
 
 interface Props {
   stores: Store[];
 }
 
 export default function Home({ stores }: Props) {
+  console.log(stores);
   return (
     <Layout>
       <main style={{ width: '100%', height: '100%' }}>
@@ -19,13 +20,20 @@ export default function Home({ stores }: Props) {
 }
 
 export async function getStaticProps() {
-  const response = await axios.get<{ stores: Store[] }>(
-    `${process.env.NEXT_API_URL}/api/stores`
-  );
+  try {
+    const response = await axios.get<{ stores: Store[] }>(
+      `${process.env.NEXT_API_URL}/api/stores`
+    );
 
-  return {
-    props: {
-      stores: response.data.stores,
-    },
-  };
+    return {
+      props: {
+        stores: response.data.stores,
+      },
+    };
+  } catch (e: unknown) {
+    // TODO : FIX ERROR TYPE
+    return {
+      props: {},
+    };
+  }
 }
