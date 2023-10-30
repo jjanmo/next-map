a5a4c1f0da10855a76bd545f64863ccdd5b758ef
jjanmo <jjanmo07@hanmail.net>
2023-10-30 23:11:33 +0900

    feat: husky test1

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,14 +1,16 @@
-import { useState } from "react"
+import { useState } from 'react';
 
-export default function Counter(){
-  const [count, setCount]= useState<number>(0)
+export default function Counter() {
+  const [count, setCount] = useState<number>(0);
 
-  const handleButtonClick = () => { 
-    setCount(count +1)
-  }
-  
-  return <div>
-    <div>{count}</div>
-    <button onClick={handleButtonClick}>+</button>
-  </div>
+  const handleButtonClick = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <div>{count}</div>
+      <button onClick={handleButtonClick}>+</button>
+    </div>
+  );
 }
