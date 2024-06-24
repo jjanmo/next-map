bdf1c043f5f90938732cae6eca8c2995cf017867
jjanmo <jjanmo07@hanmail.net>
2024-06-25 01:45:34 +0900

    feat: 네이버 지도 스크립트 랜더링 방식 변경

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -7,6 +8,11 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Script
+          src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_NCP_CLIENT_ID}`}
+          type="text/javascript"
+          strategy="beforeInteractive"
+        />
       </body>
     </Html>
   )
