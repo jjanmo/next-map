5fbeb4d34fe8f063a6a6bed5fe27df9d5094ddfe
jjanmo <jjanmo07@hanmail.net>
2025-04-10 00:57:23 +0900

    fix: naver map script url 변경 적용

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,7 @@ export default function Document() {
         <Main />
         <NextScript />
         <Script
-          src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_NCP_CLIENT_ID}`}
+          src={`https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_NCP_CLIENT_ID}`}
           type="text/javascript"
           strategy="beforeInteractive"
         />
