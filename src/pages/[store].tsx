e66d80ae2b9b8cb4edc466ac1258ec83da453cac
jjanmo <jjanmo07@hanmail.net>
2025-04-27 23:21:36 +0900

    feat: 스토어 페이지 구성(빌드타입 설정)

--- src/pages/[store].tsx
+++ src/pages/[store].tsx
@@ -1,10 +1,39 @@
-import { useRouter } from 'next/router'
+import { NextPage, GetStaticProps } from 'next'
+import { Store as StoreType } from '@/types/store'
 
-const Store = () => {
-  const router = useRouter()
-  const { store } = router.query
+interface Props {
+  store: StoreType
+}
 
-  return <div>{store} 페이지 입니다.</div>
+const Store: NextPage<Props> = ({ store }) => {
+  return <div>{store.name} 페이지 입니다.</div>
 }
 
 export default Store
+
+export const getStaticPaths = async () => {
+  const paths = (await import('../../public/stores.json')).default.map((store) => ({
+    params: { store: store.name },
+  }))
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
+  const targetStore = (await import('../../public/stores.json')).default.find(
+    (store) => store.name === params?.store
+  )
+
+  if (!targetStore) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: { store: targetStore },
+  }
+}
