7374db0e3f5705c7939c792b546936a40343884f
jjanmo <jjanmo07@hanmail.net>
2025-05-04 19:14:38 +0900

    fix: 미디어쿼리 훅 구분점 수정(현재상황에 맞게 수정)

--- src/hooks/useMediaQuery.ts
+++ src/hooks/useMediaQuery.ts
@@ -1,18 +1,20 @@
 import { useEffect, useState, useRef } from 'react'
 import { BREAKPOINTS } from '@constants/mediaQuery'
 
+/**
+ * @description 미디어쿼리 훅
+ * @returns 모바일 여부와 데스크톱 여부를 반환 { isMobile: boolean, isDesktop: boolean }
+ */
 const useMediaQuery = () => {
   const [isMobile, setIsMobile] = useState(false)
-  const [isTablet, setIsTablet] = useState(false)
   const [isDesktop, setIsDesktop] = useState(false)
   const observerRef = useRef<ResizeObserver | null>(null)
 
   useEffect(() => {
     const updateMatches = () => {
       const width = window.innerWidth
-      setIsMobile(width <= BREAKPOINTS.MOBILE)
-      setIsTablet(width > BREAKPOINTS.MOBILE && width <= BREAKPOINTS.TABLET)
-      setIsDesktop(width >= BREAKPOINTS.DESKTOP)
+      setIsMobile(width < BREAKPOINTS.TABLET) // 테블릿 미만은 모바일로 취급
+      setIsDesktop(width >= BREAKPOINTS.TABLET) // 테블릿 이상는 데스크톱으로 취급
     }
 
     updateMatches()
@@ -27,7 +29,7 @@ const useMediaQuery = () => {
     }
   }, [])
 
-  return { isMobile, isTablet, isDesktop }
+  return { isMobile, isDesktop }
 }
 
 export default useMediaQuery
